1132679518df98d039d9d3acc93e4ad03362491a
Andrias Taniwan <poporetto@gmail.com>
2022-06-28 16:18:02 +1000

    add new pricing box and new design

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,7 @@ import Server from "./pages/pricing/components/ServerPricing";
 
 function App() {
   return (
-    <div style={{ margin: "50px" }}>
-      <h1>Pricing Page</h1>
+    <div style={{ margin: "0px" }}>
       <div>
         <Cloud />
         <Server />
